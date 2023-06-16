9e18ea8f0fff52719a8279df245fcfcbde945c45
James F Moore <124608754+Kukukachuj@users.noreply.github.com>
2023-06-15 21:12:42 -0400

    Update TimeCalc.py
    Added constants DAYS HOURS MINUTES

--- TimeCalc.py
+++ TimeCalc.py
@@ -1,30 +1,35 @@
-seconds = 0
-days = 0
-hours = 0 
-minutes = 0
-sec = int(input("Enter the number of seconds you want to convert"))
+DAYS = 86400
+HOURS = 3600 
+MINUTES = 60
+
+sec = int(input("Enter the number of seconds you want to convert: "))   
+
 if sec >= 0 and sec < 60:
-    seconds = sec % 60
+    seconds = sec % MINUTES
     print(f'Seconds: ', seconds)
-elif sec >= 60 and sec < 3600:
-    minutes = sec // 60
-    seconds = sec % 60
-    print(f'Minutes: ', seconds)
+    
+elif sec >= MINUTES and sec < HOURS:
+    minutes = sec // MINUTES
+    seconds = sec % MINUTES
+    print(f'Minutes: ', minutes)
     print(f'Seconds: ', seconds)
-elif sec >= 3600 and sec  < 86400:
-    hours = sec // 3600
-    minutes = (sec % 3600) // 60
-    seconds = (sec % 3600) % 60
+    
+elif sec >= HOURS and sec  < DAYS:
+    hours = sec // HOURS
+    minutes = (sec % HOURS) // MINUTES
+    seconds = (sec % HOURS) % MINUTES
     print(f'hours: ', hours)
     print(f'Minutes: ', minutes)
     print(f'Seconds: ', seconds)
-elif sec >= 86400:
-    days = sec // 86400
-    hours = (sec % 86400) // 3600
-    minutes = ((sec % 86400) % 3600) // 60
-    seconds = ((sec % 86400) % 3600) % 60
+    
+elif sec >= DAYS:
+    days = sec // DAYS
+    hours = (sec % DAYS) // HOURS
+    minutes = ((sec % DAYS) % HOURS) // MINUTES
+    seconds = ((sec % DAYS) % HOURS) % MINUTES
     print(f'Days: ', days)
     print(f'Hours: ', hours)
     print(f'Minutes: ', minutes)
     print(f'Seconds: ', seconds)
+
      
