15f2f50d13e70d6c2d63cf45e2ddf77194efda1f
James F Moore <124608754+Kukukachuj@users.noreply.github.com>
2023-06-16 07:01:14 -0400

    Create M4CW1_GradeCalc.java
    Assignment

---
+++ M4CW1_GradeCalc.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class Main 
+{
+  public static void main(String[] args) 
+  {
+    //assign test score to a variable
+    int testScore;
+    Scanner k = new Scanner(System.in);
+    System.out.print("Enter a grade: ");
+    testScore = k.nextInt();
+
+
+    char grade;
+    if (testScore >= 90 && testScore <= 100)
+    {
+      grade = 'A';
+    }
+    else if (testScore >=80)
+    {
+      grade ='B';
+    }
+    else if (testScore >= 70)
+    {
+      grade = 'C';
+    }
+    else if (testScore >= 60)
+    {
+      grade ='D';
+    }
+    else 
+    {
+      grade = 'F';
+    }
+    System.out.println("Grade is " + grade);
+      
+  }
+}
